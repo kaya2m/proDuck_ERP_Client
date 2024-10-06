84b76622fbb6e7623722600b4a9d22ac6ca63f15
muhammet kaya <mkaya349@hotmail.com>
2024-10-06 21:38:11 +0300

    Enhance customer list component with new styles and context menu functionality

--- src/app/components/pages/customers/list-customer/list.component.ts
+++ src/app/components/pages/customers/list-customer/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Table, TableModule } from 'primeng/table';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from '../../../../services/base/base.component';
@@ -18,16 +18,21 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { CreateCustomerComponent } from '../create-customer/create-customer.component';
+import { MenuItem } from 'primeng/api';
+import { ContextMenuModule } from 'primeng/contextmenu';
 @Component({
   selector: 'customer-list',
-  templateUrl:'./list.component.html',
+  templateUrl: './list.component.html',
   styleUrl: './list.component.css',
   standalone: true,
-  imports: [CardComponent,TableModule,TagModule,IconFieldModule,InputIconModule,PaginatorModule,InputTextModule,MultiSelectModule,DropdownModule,CommonModule,ButtonModule],
+  imports: [CardComponent, TableModule, TagModule, IconFieldModule, ContextMenuModule, InputIconModule, PaginatorModule, InputTextModule, MultiSelectModule, DropdownModule, CommonModule, ButtonModule],
   providers: [DialogService],
 })
 
 export class ListComponent extends BaseComponent implements OnInit {
+  totalCount: number;
+  columns: { field: string; header: string; filterType: string; }[];
+  searchValue: any;
   constructor(
     private customerService: CustomerService,
     spinner: NgxSpinnerService,
@@ -48,10 +53,35 @@ export class ListComponent extends BaseComponent implements OnInit {
 
   activityValues: number[] = [0, 100];
 
-
+  contextMenuItems: MenuItem[] = [];
 
   async ngOnInit() {
-     await this.getCustomerList();
+    await this.getCustomerList();
+    this.columns = [
+      { field: 'code', header: 'Code', filterType: 'text' },
+      { field: 'name', header: 'Name', filterType: 'text' },
+      { field: 'paymentMethod', header: 'Payment Method', filterType: 'text' },
+      { field: 'countryCode', header: 'Country Code', filterType: 'text' },
+      { field: 'country', header: 'Country', filterType: 'text' },
+      { field: 'city', header: 'City', filterType: 'text' },
+      { field: 'town', header: 'Town', filterType: 'text' },
+      { field: 'contactNumber', header: 'Contact Number', filterType: 'text' },
+      { field: 'contactNumber2', header: 'Contact Number 2', filterType: 'text' },
+      { field: 'email', header: 'Email', filterType: 'text' },
+      { field: 'email2', header: 'Email 2', filterType: 'text' },
+      { field: 'address', header: 'Address', filterType: 'text' },
+      { field: 'address2', header: 'Address 2', filterType: 'text' },
+      { field: 'postCode', header: 'Post Code', filterType: 'text' },
+      { field: 'companyName', header: 'Company Name', filterType: 'text' },
+      { field: 'taxNumber', header: 'Tax Number', filterType: 'text' },
+      { field: 'taxOffice', header: 'Tax Office', filterType: 'text' },
+      { field: 'idNumber', header: 'ID Number', filterType: 'text' },
+      { field: 'notes', header: 'Notes', filterType: 'text' }
+    ];
+
+    this.contextMenuItems = [
+      { label: 'Delete', icon: 'pi pi-trash', command: () => this.deleteCustomer() }
+    ];
   }
 
   async getCustomerList() {
@@ -66,8 +96,32 @@ export class ListComponent extends BaseComponent implements OnInit {
     );
     if (allCustomers) {
       this.customers = allCustomers.data;
+      this.totalCount = allCustomers.totalCount;
     }
     this.loading = false;
+  }
+  async loadCustomers(event: any) {
+    this.loading = true;
+    const pageNumber = event.first / event.rows;
+    const pageSize = event.rows;
+
+    this.loading = true;
+    const allCustomers = await this.customerService.read(pageNumber, pageSize,
+      () => this.hideSpinner(SpinnerType.squareJellyBox),
+      errorMessage => this.alertifyService.message(errorMessage, {
+        dismissOther: true,
+        messageType: MessageType.Error,
+        position: Position.TopRight
+      })
+    );
+    if (allCustomers) {
+      this.customers = allCustomers.data;
+      this.totalCount = allCustomers.totalCount;
+    }
+    this.loading = false;
+  }
+  deleteCustomer() {
+
   }
 
   async pageChanged() {
@@ -113,5 +167,9 @@ export class ListComponent extends BaseComponent implements OnInit {
       footer: 'footer',
     });
   }
-
+  onGlobalFilter(table: Table, event: Event) {
+    var text = (event.target as HTMLInputElement).value
+    debugger
+    table.filterGlobal(text, 'contains');
+  }
 }
